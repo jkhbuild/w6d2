7f1b704d4b4596f9c6668a3cd3b33b68db7b2b88
jkhbuild <jkh.build@gmail.com>
2022-10-18 16:22:18 -0400

    phase 00 completed

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -3,10 +3,10 @@ class AttrAccessorObject
 
     names.each do |name|
       define_method(name) do
-        instance_variable_get(name)
+        instance_variable_get("@#{name}")
       end
       define_method("#{name}=") do |value|
-        instance_variable_set(@value, name)
+        instance_variable_set("@#{name}", value)
       end
     end
     
